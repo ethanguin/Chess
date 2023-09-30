1af9e3f5f205971947a3ea6e0d95b73d0f3b185a
Ethan Bishop <ethanielbishop@gmail.com>
2023-09-30 10:18:31 -0600

    add (chessBoard) basic array

--- src/main/chess/ChessBoardImpl.java
+++ src/main/chess/ChessBoardImpl.java
@@ -1,18 +1,22 @@
 package chess;
 
 public class ChessBoardImpl implements ChessBoard {
+    int[][] board = new int[8][8];
     @Override
     public void addPiece(ChessPosition position, ChessPiece piece) {
-
+        //cast position to
     }
 
     @Override
     public ChessPiece getPiece(ChessPosition position) {
+        //search if a piece is at that position
+        //if there is, return it, if not, return null
         return null;
     }
 
     @Override
     public void resetBoard() {
-
+        // delete all pieces
+        //create new pieces in all new positions
     }
 }
